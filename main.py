9aa158a62baa02c93acc55abfae28dcee65b903b
Giovanni <gischenato@gmail.com>
2024-03-27 13:59:27 -0300

    yaml parser

--- main.py
+++ main.py
@@ -3,25 +3,55 @@ from queue_1 import Queue
 from scheduler import Scheduler
 from simulation import Simulation
 from stats import Stats
+from pseudo_random_numbers import PseudoRandomNumbers
+from sys import argv
+import yaml
 
-arrival_time = 2
-capacity = 5
-servers = 3
-random_numbers = [0.8, 0.2, 0.1, 0.9, 0.3, 0.4, 0.7]
-scheduler = Scheduler(random_numbers)
-arrival_interval = Interval(2,5)
-service_interval = Interval(3,5)
 
-queue = Queue(capacity=capacity, servers=servers, scheduler=scheduler,arrival_interval=arrival_interval, service_interval=service_interval)
+CONFIG = {}
 
-sim = Simulation(arrival=arrival_time, queue=queue, scheduler=scheduler, random_numbers=random_numbers)
 
-sim.run()
+def load_config(file_name):
+    global CONFIG
 
-stats = Stats(queue)
+    with open(file_name) as stream:
+        try:
+            CONFIG = yaml.safe_load(stream)
+        except:
+            print('=== ERROR LOADING YAML FILE ===')
+            exit(0)
 
-stats.show_prob_distribution()
 
-stats.show_losses()
+def main():
+    global CONFIG
 
-stats.show_global_time()
+    load_config(argv[1])
+
+    ARRIVAL_TIME = CONFIG['arrivalTime']
+    CAPACITY = CONFIG['capacity']
+    SERVERS = CONFIG['servers']
+    ARRIVAL_INTERVAL = Interval(CONFIG['minArrival'], CONFIG['maxArrival'])
+    SERVICE_INTERVAL = Interval(CONFIG['minService'], CONFIG['maxService'])
+
+    SEED = CONFIG['seed']
+    random_numbers = PseudoRandomNumbers(SEED).gen_rand(CONFIG['qntRandomNumbers'])
+
+    SCHEDULER = Scheduler(random_numbers)
+
+    queue = Queue(capacity=CAPACITY, servers=SERVERS, scheduler=SCHEDULER, arrival_interval=ARRIVAL_INTERVAL, service_interval=SERVICE_INTERVAL)
+
+    sim = Simulation(arrival=ARRIVAL_TIME, queue=queue, scheduler=SCHEDULER, random_numbers=random_numbers)
+
+    sim.run()
+
+    stats = Stats(queue)
+
+    stats.show_prob_distribution()
+
+    stats.show_losses()
+
+    stats.show_global_time()
+
+
+if __name__ == '__main__':
+    main()
